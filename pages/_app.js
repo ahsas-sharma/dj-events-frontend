44a1013c35e0626921990305bb1387422feb4166
Ahsas Sharma <sharma.ahsas@gmail.com>
2022-09-16 08:04:04 +0530

    Pages and routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  // If the component below is wrapped in empty tags and another component is added above it, that will show up on all pages. 
   return <Component {...pageProps} />
 }
 
